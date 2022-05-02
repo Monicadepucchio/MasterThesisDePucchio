5ea7fdae3a8f1edbf20f4554f9ed2eb263a92138
Monicadepucchio <95436459+Monicadepucchio@users.noreply.github.com>
2022-05-02 16:36:54 +0200

    Add files via upload

--- Init_Parameters.m
+++ Init_Parameters.m
@@ -24,3 +24,31 @@ KzWhlAxl= data.KzWhlAxl ; %[N/m] Wheel and axle interface compliance constant
 F0zCS=data.F0z(2); %[N] Suspension spring preload
 F0zWhlAxl= data.F0zWhlAxl; %[N] Wheel and axle interface compliance preload
 
+%% Trasmission Parameters
+Trans=data.Trans;
+
+%% Engine Parameters
+f_tbrake_t_bpt=data.f_tbrake_t_bpt; %[Nm] Breakpoints for commanded torque input
+f_tbrake_n_bpt=data.f_tbrake_n_bpt; %[rpm] Breakpoints for engine speed input
+NCyl=data.NCyl; % Number of cylinders
+Cps=data.Cps; % Crank revolutions per power stroke
+Vd=data.Vd; %[m^3] Total displaced volume
+Rair=data.Rair; %[J/(kg*K)] Ideal gas constant air
+Pstd=data.Pstd; %[Pa] Air standard pressure
+Tstd=data.Tstd; %[K] Air standard temperature
+
+f_tbrake=data.f_tbrake; %[Nm] Brake torque map
+f_air=data.f_air; %[kg/s] Air mass flow map
+f_fuel=data.f_fuel;%[kg/s] Fuel flow map
+f_texh=data.f_texh; %[K] Exhaust temperature map
+f_eff=data.f_eff; %[g/Kwh] BSFC map
+f_hc=data.f_hc; %[kg/s] EO HC map
+f_co=data.f_co; %[kg/s] EO CO map
+f_nox=data.f_nox; %[kg/s] EO NOx map
+f_co2=data.f_co2; %[kg/s] EO CO2 map
+f_pm=data.f_pm; %[kg/s] EO PM map
+
+% Accessory load power
+AccPwrbp= [0 3.0779560993538198];
+AccSpdbp= [256.49641719585139 641.24104298962845];
+AfrStoich= 14.6;
